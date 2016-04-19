e99327a3b34f57f64dbc7300d588129f67ca3ace
Adriana Castaneda <adriana.lcs316@gmail.com>
2016-04-19 09:31:06 -0500

    add patient count by year function

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,4 +4,17 @@ class Participant < ActiveRecord::Base
   has_many :sites, through: :enrollments
 
   validates :name, :gender, :birthdate, presence: true
+
+  def self.patient_count_by_birthdate(year)
+    participants= Participant.all
+    count = 0
+    participants.each do |participant|
+      if participant.birthdate.year == year
+          count += 1
+      end
+    end
+    p count
+  end
 end
+
+
