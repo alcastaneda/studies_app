c944dd8f5ca59c54da5eebfbc0d47a1c596d9dd6
Adriana Castaneda <adriana.lcs316@gmail.com>
2016-04-19 10:27:39 -0500

    add csv functionality

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Site < ActiveRecord::Base
   has_many :study_site_joins
   has_many :studies, through: :study_site_joins
@@ -5,4 +7,14 @@ class Site < ActiveRecord::Base
   has_many :participants, through: :enrollments
 
   validates :name, :location, presence: true
+
+  def self.export_participants(site_id)
+    site = Site.find(site_id)
+    participants= site.participants
+    CSV.open("#{site.name}.csv", "a+") do |csv|
+        participants.each do |participant|
+            csv << [participant.name, participant.gender, participant.birthdate, participant.address]
+        end
+    end
+  end
 end
